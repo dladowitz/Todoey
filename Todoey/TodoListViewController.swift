adf1a580239c39315e7fa36cb0e0138a17dba59e
David Ladowitz <david@ladowitz.com>
2018-04-18 08:34:45 -0700

    Storing data with NSCoder

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,22 +10,27 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demagorgan"]
+    var itemArray = [Item]()
     let defaults = UserDefaults.standard
+
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory,
+                                                in: .userDomainMask).first?.appendingPathComponent("Items.plist")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
+        loadItems()
     }
 
-
+    // Create cells on screen
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+
+        let item = itemArray[indexPath.row]
+
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
 
         return cell
     }
@@ -35,16 +40,12 @@ class TodoListViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
         print("Clicked on: \(itemArray[indexPath.row])")
 
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-           tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
 
         tableView.deselectRow(at: indexPath, animated: true)
+        saveItems()
     }
 
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
@@ -54,25 +55,46 @@ class TodoListViewController: UITableViewController {
 
         let action = UIAlertAction(title: "Add Item", style: .default) { 
             (action) in
-            print("Success: \(textField.text)")
-            self.itemArray.append(textField.text!)
 
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            print("Success: \(String(describing: textField.text))")
+            self.itemArray.append(Item(title: textField.text!, done: false))
 
-            self.tableView.reloadData()
+            self.saveItems()
         }
 
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create New Item"
             textField = alertTextField
-
         }
+
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
 
+    func saveItems() {
+        let encoder = PropertyListEncoder()
 
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        }  catch {
+            print("Error \(error)")
+        }
 
+        self.tableView.reloadData()
+    }
 
+    func loadItems(){
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("itemsArray: \(itemArray)")
+            }
+
+        }
+    }
 }
 
